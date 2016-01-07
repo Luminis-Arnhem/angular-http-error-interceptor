7e1430ecf49c49e85134308ea2a476ff60107e5b
frankf <frank.folsche@luminis.eu>
2016-01-07 10:46:29 +0100

    Initial check in of the interceptor

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+﻿var gulp = require('gulp');
+var merge = require('merge2');
+var p = require('gulp-load-plugins')();
+
+var src = 'src';
+var appVersion = '0.1.0';
+var output = 'dist';
+var typesSrc = 'typings';
+var tsScriptFiles = [typesSrc + '/**/*.ts', src + '/**/*.ts'];
+var versionFiles = ['./bower.json', './package.json'];
+
+var tsProject = p.typescript.createProject({
+    declaration: true,
+    noExternalResolve: true,
+    module: "system",
+    target: "ES5",
+    sortOutput: true
+});
+
+var copy = function (source) {
+    return gulp.src(source)
+        .pipe(gulp.dest(output));
+};
+
+var compileTS = function () {
+    var tsResult = gulp.src(tsScriptFiles)
+        .pipe(p.replace('/// <reference path="../typings/angularjs/angular.d.ts" />', '//Angular definition file removed'))
+        .pipe(p.typescript(tsProject));
+    tsResult.dts
+        .pipe(gulp.dest(output + '/typings'));
+    tsResult.js
+        .pipe(p.ngAnnotate())
+        .pipe(p.concat('angular-value-not-in-objects.js'))
+        .pipe(gulp.dest(output + '/js'));
+}
+gulp.task('compileTS', function () {
+    return compileTS();
+});
+gulp.task('clean', function () {
+    return gulp.src([output], { read: false })
+		.pipe(p.clean());
+});
+gulp.task('setVersion', function () {
+    gulp.src(versionFiles)
+        .pipe(p.bump({ version: appVersion }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['clean', 'setVersion'], function () {
+    return gulp.start('compileTS');
+});
---
+++ src/angular-http-error-interceptor.ts
@@ -0,0 +1,60 @@
+﻿/// <reference path="../typings/angular-translate/angular-translate.d.ts" />
+/// <reference path="../typings/angularjs-toaster/angularjs-toaster.d.ts" />
+/// <reference path="../typings/angularjs/angular.d.ts" />
+var module: angular.IModule = angular.module('http-error-interceptor', ['translations', 'toastr']);
+module Common {
+    export interface IInterceptor {
+        request: Function;
+        requestError: Function;
+        response: Function;
+        responseError: Function;
+    }
+
+    export class HttpErrorInterceptor implements IInterceptor {
+        public static $inject = ["$injector", "$q", "toastr", "$filter"];
+
+        public static Factory($injector: angular.auto.IInjectorService, $q: angular.IQService, toastr: any, $filter: angular.IFilterService) {
+            return new HttpErrorInterceptor($injector, $q, toastr, $filter);
+        }
+
+        constructor(private $injector: angular.auto.IInjectorService, private $q: angular.IQService, private toastr: any, private $filter: angular.IFilterService) {
+        }
+
+        public request = (requestSuccess): angular.IPromise<any> => {
+            return requestSuccess;
+        }
+        public requestError = (requestFailure): angular.IPromise<any> => {
+            var generalMessage: string = this.$filter("translate")("CONNECTION_ERROR");
+            var title: string = this.$filter("translate")("SERVER_ERROR_TITLE");
+            var message: string = requestFailure.data.message || this.$filter("translate")("UNKNOWN_ERROR");
+
+            var specificMessage: string = generalMessage + message;
+            this.toastr.error(specificMessage, title);
+            return this.$q.reject(requestFailure);
+        }
+        public response = (responseSuccess): angular.IPromise<any> => {
+            return responseSuccess;
+        }
+        public responseError = (responseFailure): angular.IPromise<any> => {
+            if (responseFailure.status === 401) {
+                this.toastr.warning(this.$filter("translate")("LOGGED_OUT_BY_SYSTEM"));
+                window.location.reload(true);
+            } else {
+                var generalMessage: string = this.$filter("translate")("SERVER_ERROR");
+                var title: string = this.$filter("translate")("SERVER_ERROR_TITLE");
+                var message: string = responseFailure.data.message || this.$filter("translate")("UNKNOWN_ERROR");
+                var specificMessage: string = generalMessage + message;
+                this.toastr.error(specificMessage, title, {
+                    timeOut: 0,
+                    extendedTimeOut: 0,
+                    closeButton: true
+                });
+            }
+            return this.$q.reject(responseFailure);
+        }
+    }
+};
+module.factory("httpErrorInterceptor", ["$injector", "$q", "toastr", "$filter", Common.HttpErrorInterceptor.Factory]);
+module.config(($httpProvider: angular.IHttpProvider) => {
+    $httpProvider.interceptors.push('httpErrorInterceptor');
+});
