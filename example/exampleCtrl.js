12b648d421e1a83ab41902a670bc79cc760bdc4d
Koen Barmentlo <koen.barmentlO@luminis.eu>
2016-04-08 17:08:53 +0200

    Added two extra buttons in example.

--- example/exampleCtrl.js
+++ example/exampleCtrl.js
@@ -6,10 +6,26 @@ var app;
             this.$http = $http;
             this.toastr = toastr;
         }
-        ExampleCtrl.prototype.doRequest = function () {
+        ExampleCtrl.prototype.do404Request = function () {
             this.$http({
                 method: 'get',
                 url: window.location.protocol + '/does/not/exist',
+            }).then(function (response) {
+            });
+        };
+        ;
+        ExampleCtrl.prototype.do401Request = function () {
+            this.$http({
+                method: 'get',
+                url: 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139',
+            }).then(function (response) {
+            });
+        };
+        ;
+        ExampleCtrl.prototype.do400Request = function () {
+            this.$http({
+                method: 'get',
+                url: 'https://www.googleapis.com/books/v1/users/userId/bookshelves',
             }).then(function (response) {
             });
         };
--- example/exampleCtrl.ts
+++ example/exampleCtrl.ts
@@ -9,10 +9,26 @@ namespace app {
 
         }
 
-        doRequest() {
+        do404Request() {
             this.$http({
                 method: 'get',
                 url: window.location.protocol + '/does/not/exist',
+            }).then((response) => {
+            });
+        };
+
+        do401Request() {
+            this.$http({
+                method: 'get',
+                url: 'http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139',
+            }).then((response) => {
+            });
+        };
+
+        do400Request() {
+            this.$http({
+                method: 'get',
+                url: 'https://www.googleapis.com/books/v1/users/userId/bookshelves',
             }).then((response) => {
             });
         };
